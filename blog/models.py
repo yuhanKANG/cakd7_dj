8ca40838d04c12d88faa9d886f03927650633a0c
yuhanKANG <overlight1@gmail.com>
2022-08-04 18:49:23 +0900

    과제겸 복습

--- blog/models.py
+++ blog/models.py
@@ -8,4 +8,4 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self) :
-        return f'{self.pk}) {self.title}'
+        return f'{self.pk}){self.title}'
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
