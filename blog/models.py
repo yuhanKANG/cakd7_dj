d80f7a842755d516921efd27b760a6735895299f
yuhanKANG <overlight1@gmail.com>
2022-08-12 17:50:45 +0900

    수정

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,36 @@
-from django.db import models
 import os
+from django.db import models
+from django.contrib.auth.models import User
+from markdownx.models import MarkdownxField
+from markdownx.utils import markdown
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}/'
+        
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+
+    class Meta:
+        verbose_name_plural = 'categories'
+        
 class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
-    # hook(요약문 필드) 100글자까지 제한
-    content = models.TextField()
+    content = MarkdownxField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
@@ -13,9 +38,16 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
-    def __str__(self) :
-        return f'{self.pk}){self.title}'
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    
+    tags = models.ManyToManyField(Tag, blank=True)
+   
+    # author: 추후 작성 예정
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
+  
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
     
@@ -24,3 +56,22 @@ class Post(models.Model):
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
+
+    def get_content_markdown(self):
+        return markdown(self.content)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author}::{self.content}'
+
+    def get_absolute_url(self):
+        return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+    
+    
+    
