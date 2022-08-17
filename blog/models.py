2d2b87fd50df9836643aa32aec9347c8b4e997ea
yuhanKANG <overlight1@gmail.com>
2022-08-17 14:24:33 +0900

    수정

--- blog/models.py
+++ blog/models.py
@@ -60,6 +60,12 @@ class Post(models.Model):
     def get_content_markdown(self):
         return markdown(self.content)
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/1242/69e8e839d856a4f9/svg/{self.author.email}'
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
